cd9f2151e8cf66fc9f97656df5505b603475bbdc
Alumno <Alumno@PC11>
2019-08-23 10:08:58 -0500

    Nuevo ciclo for

---
+++ Ciclo for/Ciclo for/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Dame los números del X hasta Y");
+            Console.WriteLine("Ingresa el valor de x");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa el valor de y");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De cuanto va a ir la secuencia");
+            z = Convert.ToInt32(Console.ReadLine());
+            if (x > y)
+            {
+                for (int i = x; i > y; i -= z)
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+            else
+            {
+                for (int i = x; i < y; i += z)
+                {
+                    Console.WriteLine("i = " + i);
+                }
+            }
+        }
+    }
+}
